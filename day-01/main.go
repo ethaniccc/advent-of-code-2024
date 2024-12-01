34d0a2cfc85169b1ae1c5bf42711704744f14e7d
ethaniccc <schooldocumentsandstuff@gmail.com>
2024-12-01 18:58:29 -0500

    add part 2 of day 1

--- day-01/main.go
+++ day-01/main.go
@@ -24,8 +24,8 @@ func parse_input() {
 	for index, line := range strings.Split(string(dat), "\n") {
 		// Split the line from the whitespace inbetween the left and right lists.
 		split := strings.Fields(line)
-		left_num, _ := strconv.ParseUint(split[0], 10, 64)
-		right_num, _ := strconv.ParseUint(split[1], 10, 64)
+		left_num, _ := strconv.ParseInt(split[0], 10, 64)
+		right_num, _ := strconv.ParseInt(split[1], 10, 64)
 
 		// Put each parsed integer into the left & right list.
 		left_list[index] = int32(left_num)
@@ -33,15 +33,9 @@ func parse_input() {
 	}
 }
 
-func main() {
-	// Parse the input and fill the left/right lists.
-	parse_input()
-
-	// Sort both the slices using the slices package.
-	slices.Sort(left_list)
-	slices.Sort(right_list)
-
+func calculate_distance() int32 {
 	var total_distance int32
+
 	for index := 0; index < 1_000; index++ {
 		left_num := left_list[index]
 		right_num := right_list[index]
@@ -54,5 +48,45 @@ func main() {
 		total_distance += distance
 	}
 
-	fmt.Println(total_distance)
+	return total_distance
+}
+
+func calculate_similarity() uint64 {
+	var (
+		apperances       = make(map[int32]uint16)
+		similarity_score uint64
+	)
+
+	// First iterate through the right list, to find the amount of times certain numbers appear
+	// and put them on the apperances map.
+	for _, num := range right_list {
+		if _, ok := apperances[num]; !ok {
+			apperances[num] = 1
+			continue
+		}
+
+		apperances[num]++
+	}
+
+	// Now, iterate through the left list to see how many times a certain number appears on the
+	// right list, and add to the similarity score the times appeared multiplied by the number itself.
+	for _, num := range left_list {
+		if times_appeared, ok := apperances[num]; ok {
+			similarity_score += uint64(times_appeared) * uint64(num)
+		}
+	}
+
+	return similarity_score
+}
+
+func main() {
+	// Parse the input and fill the left/right lists.
+	parse_input()
+
+	// Sort both the slices using the slices package.
+	slices.Sort(left_list)
+	slices.Sort(right_list)
+
+	fmt.Println("distance:", calculate_distance())
+	fmt.Println("similarity:", calculate_similarity())
 }
